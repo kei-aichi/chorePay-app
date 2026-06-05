b169a0288ee3eaaf210b58d4eb474330462c932f
keiichiro shikai <nb.jtktn.1211@gmail.com>
2026-06-05 21:33:06 +0900

    feat: Factory.Seeder実装

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Task;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +17,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::factory()->create([
+            'name' => 'テストユーザー',
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Category::factory(5)
+            ->for($user)
+            ->create()
+            ->each(function ($category) use ($user) {
+
+                Task::factory(5)
+                    ->create([
+                        'user_id' => $user->id,
+                        'category_id' => $category->id,
+                    ]);
+            });
     }
 }
